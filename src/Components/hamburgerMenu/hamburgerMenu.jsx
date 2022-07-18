bc5c5dba21c99a504bed9f5c39a0a64ef807b0cc
Iliya <mrdonev@gmail.com>
2022-07-18 15:10:18 +0100

    halfway fix the hamburger menu

--- src/Components/hamburgerMenu/hamburgerMenu.jsx
+++ src/Components/hamburgerMenu/hamburgerMenu.jsx
@@ -4,19 +4,19 @@ const HamburgerMenu = () => {
 
 
   return (
-    <>
+    <div id='hamburger-menu'>
     <input id="menu-toggle" type="checkbox" />
     <label className='menu-button-container' htmlFor="menu-toggle">
     <div className='menu-button'></div>
   </label>
     <ul className="menu">
-      <li>One</li>
-      <li>Two</li>
-      <li>Three</li>
-      <li>Four</li>
-      <li>Five</li>
+     <li> <a href='#home'>Home</a></li>
+      <li><a href='#about'>About</a></li>
+      <li><a href='#projects'>Projects</a></li>
+      <li><a href='#contacts'>Contacts</a></li>
+   
     </ul>
-    </>
+    </div>
   );
 };
 
